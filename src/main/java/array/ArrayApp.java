1e122846029432f0f20460e564eb52d38fbf5442
Yana Chorna <johanna1988@ukr.net>
2022-08-04 10:45:43 +0300

    Added the updated missing file

--- src/main/java/array/ArrayApp.java
+++ src/main/java/array/ArrayApp.java
@@ -1,16 +1,16 @@
 package main.java.array;
 
-import main.java.array.file.OperationsWithArrays;
+import main.java.array.file.ActionsWithArrays;
 
 public class ArrayApp {
+
     public static void main(String[] args) {
 
-        OperationsWithArrays results = new OperationsWithArrays();
-        results.removeDivisibleByThree();
-        results.replaceString();
-        results.defineAverage();
-        results.sortInDescOrder();
-        results.sumUpPositiveNum();
-
+        ActionsWithArrays result = new ActionsWithArrays();
+        result.defineAverage();
+        result.sumUpPositiveNum();
+        result.removeDivisibleByThree();
+        result.sortInDescOrder();
+        result.replaceString();
     }
 }
